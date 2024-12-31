a206db9819e55259ccae44d63cb0a180007b0a8c
hersheyar <hersheyar@outlook.com>
2024-12-30 19:40:02 -0600

    added a footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import Navbar from "./components/navbar.jsx"
+import Footer from "./components/footer.jsx"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <Navbar></Navbar>
-      <h1>Hello from React!</h1>
+        <Navbar />
+        <h1>Hello from React!</h1>
+        <Footer />
     </div>
   );
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import "./styles/footer.css"
+
+function Footer() {
+    return (
+        <div>
+            <footer>Online Store created By Hershand</footer>
+        </div>
+    );
+}
+
+export default Footer;
